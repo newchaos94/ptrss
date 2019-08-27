db91d1ed30d530107bcd4190c88b8fe8c1ccd39b
TonyLiu <liuchao8794@gmail.com>
2019-08-27 10:57:20 +0800

    添加标题判断

--- ptrss.py
+++ ptrss.py
@@ -19,9 +19,8 @@ import yaml
 
 
 class RssDB():
-    def __init__(self, path=None):
-        if path is None:
-            path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'rss.db')
+    def __init__(self):
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'rss.db')
         self.conn = sqlite3.connect(path)
         self.csr = self.conn.cursor()
 
@@ -30,19 +29,18 @@ class RssDB():
         初始化sqlite
         '''
         #self.csr.execute("drop table torrents")
-        cnt = self.csr.execute("select count(*) from sqlite_master where type='table' and name = 'torrents';")
+        cnt = self.csr.execute("select count(*) from sqlite_master where type='table' and name='torrents';").fetchone()[0]
         if cnt == 0:
             self.csr.execute(''' create table torrents (
                 id          text primary key not null,
                 title       text not null,
                 size      real  not null,
-                discount    text,
                 gmt_create  timestamp  not null
             );''')
 
     def insert(self, data):
         try:
-            self.csr.execute("insert into torrents values(?,?,?,?,?)", data)
+            self.csr.execute("insert into torrents values(?,?,?,?)", data)
             self.conn.commit()
         except Exception as err:
             self.conn.rollback()
@@ -60,12 +58,12 @@ class RssDB():
     def close(self):
         try:
             self.csr.close()
-        except Exception as err:
-            print(err)
+        except Exception:
+            pass
         try:
             self.conn.close()
-        except Exception as err:
-            print(err)
+        except Exception:
+            pass
         
 
 class Rss():
@@ -78,12 +76,14 @@ class Rss():
         'halfdown': '50%',
         'twouphalfdown': '2x50%'
     }
+
     def __init__(self, config):
         self.config = dict(config)
-        if self.config.get('download') is None:
+        if not self.config.get('download'):
             self.config['download'] = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'torrents')
         self.config.setdefault('userAgent', '')
         self.config.setdefault('discount', None)
+        self.config.setdefault('title', None)
         self.config.setdefault('size', {'min': 0, 'max': -1})
         self.config['size'].setdefault('min', 0)
         self.config['size'].setdefault('max', -1)
@@ -97,29 +97,43 @@ class Rss():
     def _request(self, url):
         resp = requests.get(url, headers=self.headers, verify=False, allow_redirects=False, timeout=30)
         if resp.status_code == 302:
-            raise SyntaxError("网站无法登陆, 更新cookie~~")
+            raise RuntimeError("网站无法登陆, 更新cookie~~")
         return resp
 
-
-    def _info(self, entry):
+    def _check(self, entry):
         print('----------------{}----------------'.format(entry.title))
 
-        eid = entry.id
-        if 1 == rssdb.scale(eid):
-            raise RuntimeError()
+        # 查询数据库
+        if 1 == rssdb.scale(entry.id):
+            return False
+        # 新增
+        rssdb.insert((entry.id, entry.title, entry.links[1]['length'], time.time()))
 
-        # 获取文件大小 GB 保留1位小数
-        size = round(int(entry.links[1]['length']) / self.UNIT, 1)
-        
-        # 获取优惠信息
-        resp = self._request(entry.link)
-        html = etree.HTML(resp.text)
-        result = html.xpath('//h1[@id="top"]/b/font/@class')
-        if result:
-            discount = self.DISCOUNT.get(result[0], None)
+        # 判断标题
+        if self.config['title']:
+            flag = False
+            for title in self.config['title']:
+                if title in entry.title:
+                    flag = True
+                    break
+            if not flag:
+                print('标题不匹配')
+                return False
+
+        # 获取文件大小 GB 两位小数
+        size = round(int(entry.links[1]['length']) / self.UNIT, 2)
+        print(size)
+        max_ = self.config['size']['max']
+        min_ = self.config['size']['min']
+        if -1 == max_:
+            if size < min_:
+                print('文件过小')
+                return False
         else:
-            discount = None
-
+            if size < min_ or size > max_:
+                print('文件大小不匹配')
+                return False
+        
         # # //tr/text()
         # result = html.xpath('//td[@id="outer"]/table[1]/tr')
         # print(result)
@@ -129,40 +143,26 @@ class Rss():
         # print('---------------------------')
         # print(result[2].xpath('td[2]/text()'))
 
-        # 获取peer信息
+        # peer信息
         # result = html.xpath('//div[@id="peercount"]/b/text()')
         # print(re.search(r'(\d+)\w*', result[0], re.S).group(1))
         # print(re.search(r'(\d+)\w*', result[1], re.S).group(1))
 
-        rssdb.insert((eid, entry.title, size, discount, time.time()))
-        return size, discount
-
-    def _check(self, entry):
-        try:
-            size, discount = self._info(entry)
-            print(size, discount)
-            
-            if self.config['discount']:
+        # 优惠信息
+        if self.config['discount']:
+            resp = self._request(entry.link)
+            html = etree.HTML(resp.text)
+            result = html.xpath('//h1[@id="top"]/b/font/@class')
+            print(result)
+            if result:
+                discount = self.DISCOUNT.get(result[0], None)
                 if discount not in self.config['discount']:
                     print('优惠信息不匹配')
                     return False
-
-            max_ = self.config['size']['max']
-            min_ = self.config['size']['min']
-            if -1 == max_:
-                if size < min_:
-                    print('文件过小')
-                    return False
             else:
-                if size < min_ or size > max_:
-                    print('文件大小不匹配')
-                    return False
-            return True
-        except SyntaxError:
-            raise
-        except Exception as err:
-            print(err)
-            return False
+                print('无优惠信息')
+                return False
+
 
     def download(self):
         resp = self._request(self.config['rss'])
@@ -171,11 +171,11 @@ class Rss():
             time.sleep(random.randint(1,4))
             if self._check(e):
                 resp = self._request(e['links'][1]['href'])
-                if resp.status_code == 200 and resp.headers.get('Content-Disposition'):
-                    with open(os.path.join(self.config['download'], e.id + '.torrent'), "wb") as f:
-                        f.write(resp.content)
-                else:
-                    print(resp.status_code, resp.text)
+            #     if resp.status_code == 200 and resp.headers.get('Content-Disposition'):
+            #         with open(os.path.join(self.config['download'], e.id + '.torrent'), "wb") as f:
+            #             f.write(resp.content)
+            #     else:
+            #         print(resp.status_code, resp.text)
     
 
 if __name__ == '__main__':
@@ -189,17 +189,18 @@ if __name__ == '__main__':
         raise RuntimeError("请正确配置yaml文件")
 
     rssdb = RssDB()
-    rssdb.init_db()
-    # print(rssdb.fetchall())
-    
-    for k, v in tasks.items():
-        print('----------------{}----------------'.format(k))
-        if v.get('rss') is None or v.get('cookie') is None:
-            raise RuntimeError("任务[{}] 需要'rss', 'cookie'配置项".format(k))
-        try:
-            rss = Rss(v)
-            rss.download()
-        except Exception as err:
-            print(k, err)
-    
-    rssdb.close()
+    try:
+        rssdb.init_db()
+        # print(rssdb.fetchall())
+        
+        for k, v in tasks.items():
+            print('----------------{}----------------'.format(k))
+            if v.get('rss') is None or v.get('cookie') is None:
+                raise RuntimeError("任务[{}] 需要'rss', 'cookie'配置项".format(k))
+            try:
+                rss = Rss(v)
+                rss.download()
+            except Exception as err:
+                print(k, err)
+    finally:
+        rssdb.close()
